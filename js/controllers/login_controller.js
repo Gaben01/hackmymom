1b635be3008bc6d8d376f029a218b03ccaa7acdb
Gabriele Francia <ice.gabri@live.com>
2018-12-16 04:57:33 +0100

    lol
    4 alex

--- js/controllers/login_controller.js
+++ js/controllers/login_controller.js
@@ -1,3 +1,5 @@
 loginController = {
-
+    setUpLogin: function(){
+        
+    }
 }
--- js/services/service_form_reader.js
+++ js/services/service_form_reader.js
@@ -1,5 +1,8 @@
 formReader = {
     //qui si può aggiungere la validazione dei campi
+    validateForm: function(){
+
+    },
     getFormData: function () {
         return $(this).serialize();
     }
